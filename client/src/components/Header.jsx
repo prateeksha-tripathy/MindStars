ebf4a6b6ece5280d967ed7f7330df832b09c8b1a
Prateeksha Tripathy <prateekshatripathy10@gmail.com>
2024-05-02 22:53:50 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import { Link , useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
+import { FaMoon, FaSun } from 'react-icons/fa'
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {toggleTheme} from '../redux/theme/themeSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
     const {currentUser} = useSelector(state => state.user)
+    const dispatch = useDispatch();
+    const {theme} = useSelector(state => state.theme); 
+
   return (
     <Navbar className='border-b-2 bg-blue-300'>
         <Link to = '/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -26,8 +30,8 @@ export default function Header() {
             <AiOutlineSearch />
         </Button>
         <div className='flex gap-2 md:order-2'>
-            <button className='w-12 h-10  sm:inline' color='gray' pill="true">
-                <FaMoon />
+            <button className='w-12 h-10 rounded-full px-4 bg-white sm:inline' color='gray' pill="true" onClick={() => dispatch(toggleTheme())}>
+                 {theme === 'light' ? <FaMoon /> : <FaSun />}
             </button>
             {currentUser ? (
                 <Dropdown arrowIcon= {false} inline label = { <Avatar alt='user' img = {currentUser.profilePicture} rounded />}>
@@ -43,8 +47,7 @@ export default function Header() {
                 </Dropdown>
             ) : (
                 <Link to ='/sign-in' >
-                <Button gradientDuoTone='purpleToBlue' outline > Sign In
-                </Button>
+                <Button gradientDuoTone='purpleToBlue' outline > Sign In</Button>
             </Link>
             )}
             
