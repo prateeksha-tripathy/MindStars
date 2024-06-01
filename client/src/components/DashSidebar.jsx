13954022a035d9337e52916a05e98144b84ef61f
Prateeksha Tripathy <prateekshatripathy10@gmail.com>
2024-06-02 00:46:27 +0530

    posts options in dashsidebar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,53 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import { Sidebar } from 'flowbite-react'
-import {HiArrowSmRight, HiUser} from 'react-icons/hi'
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { signoutSuccess } from '../redux/user/userSlice';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function DashSidebar() {
-    const loaction = useLocation();
-    const dispatch = useDispatch();
-    const [tab, setTab] = useState("");
-    useEffect(() => {
-      const urlParams = new URLSearchParams(loaction.search);
-      const tabFromUrl = urlParams.get("tab");
-      if (tabFromUrl){
-        setTab(tabFromUrl)
-      }
-    }, [loaction.search]);
-
-    const handleSignout = async () => {
-      try {
-        const res = await fetch('/api/user/signout', {
-          method : 'POST',
-        })
-        const data = await res.json();
-        if (!res.ok) {
-          console.log(data.message);
-        } else{
-          dispatch(signoutSuccess());
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
+  const loaction = useLocation();
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state)=>state.user);
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(loaction.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
+  }, [loaction.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
-      < Sidebar className="w-full md:w-56" >
-        <Sidebar.Items >
-            <Sidebar.ItemGroup>
-                <Link to= '/dashboard?tab=profile'>
-                    <Sidebar.Item active={tab==='profile'} icon = {HiUser} label={"User"} labelColor='dark' as = 'div'>
-                        Profile
-                    </Sidebar.Item>
-                </Link>
-                <Sidebar.Item active icon = {HiArrowSmRight} className='cursor-pointer' onClick = {handleSignout} >
-                    Sign Out
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={currentUser.isAdmin ? "Admin" : "User"}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (                         
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Sidebar.Item
+            active
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
